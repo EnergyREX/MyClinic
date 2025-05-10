564d72e45ff983116d47d3447a6d7deed5f8efed
EnergyREX <energy.rex.2003@gmail.com>
2025-05-10 16:22:19 +0200

    del: unused controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -15,11 +15,15 @@ Route::prefix('auth')->controller(AuthController::class)->group(function () {
 
 // Protected Routes.
 Route::middleware('validateToken')->group(function() {
-  Route::post('/logout', 'logout')->name('logout');
   
+  // Logout route
+  Route::prefix('auth')->controller(AuthController::class)->group(function () {
+    Route::post('/logout', 'logout')->name('logout');
+  });
+
   // Appointment Routes.
   Route::prefix('appointments')->middleware('auth:api')->controller(AppointmentController::class)->group(function () {
-    Route::get('/', 'index')->middleware('permission:view_appointments');
+    Route::get('/', 'index')->middleware('permission:view_appointm<ents');
     Route::post('/', 'store')->middleware('permission:create_appointment');
   });
 
