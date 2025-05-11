2972d29f8848d9c4433d75e13b97861a6f566e61
EnergyREX <energy.rex.2003@gmail.com>
2025-05-11 15:32:08 +0200

    add: permissions to roles

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -25,10 +26,37 @@ class DatabaseSeeder extends Seeder
             $allRoles[$roleName] = Role::firstOrCreate(['name' => $roleName]);
         }
 
+        $permissions = [
+            'view_appointments',
+            'view_roles',
+            'view_inventories',
+            'view_suppliers',
+            'view_doctors',
+            'view_statuses',
+            'view_medicalrecords',
+            'view_medicalrecords-Own',
+            'view_treatments'
+        ];
+
+        foreach ($permissions as $perm) {
+            $allPermissions[$perm] = Permission::firstOrCreate(['name' => $perm]);
+        }
+
+        $allRoles['admin']->permission()->sync(array_values($allPermissions));
+
+        $allRoles['user']->permission()->sync([
+            $allPermissions['view_appointments']->id,
+            $allPermissions['view_inventories']->id,
+            $allPermissions['view_medicalrecords']->id,
+            $allPermissions['view_roles']->id,
+            $allPermissions['view_treatments']->id,
+        ]);
+
         // User::factory(10)->create();
 
         User::factory()->create([
             'email' => 'test@example.com',
+            'password' => 'password'
         ]);
     }
 }
