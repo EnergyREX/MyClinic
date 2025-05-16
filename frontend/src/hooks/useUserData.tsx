e7fef7e86804c1e5f04d612e1175bf8c8b290277
EnergyREX <energy.rex.2003@gmail.com>
2025-05-16 23:40:58 +0200

    feat: recoverPermissions function

---
+++ frontend/src/hooks/useUserData.tsx
@@ -0,0 +1,62 @@
+import { redirect } from 'react-router-dom'
+import { create } from 'zustand'
+
+interface State {
+  permissions: string[]
+  addPermission: (perm: string) => void
+  clearPermissions: () => void
+  recoverPermissions: () => Promise<void>
+}
+
+const useUserData = create<State>((set, get) => ({
+  permissions: [],
+
+  addPermission: (perm: string) =>
+    set((state) => (
+      state.permissions.includes(perm)
+      ? state 
+      : {permissions: [...state.permissions, perm]
+    })),
+
+  clearPermissions: () =>
+    set(() => ({
+      permissions: []
+    })),
+
+  recoverPermissions: async () => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      try {
+      const response = await fetch('http://localhost:8000/api/auth/permissions', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+      })
+
+      const data = await response.json()
+      const permissions = data.permissions || []
+
+      if (response.status === 401) {
+        localStorage.clear()
+        redirect('/')
+      }
+
+      permissions.forEach((perm: string) => {
+        get().addPermission(perm)
+      })
+
+    } catch (err) {
+      console.error('Error recovering permissions:', err)
+    }
+    } else {
+      redirect('/login')
+    }
+  }
+}))
+
+export default useUserData
