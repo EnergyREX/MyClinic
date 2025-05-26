d1955dd06971f71fa6cabfdc7b145b31422d5cbd
EnergyREX <energy.rex.2003@gmail.com>
2025-05-26 06:49:08 +0200

    refactor: routes in <App />

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import Register from './template/Register.tsx';
-import Login from './template/Login.tsx';
-
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import NotFound from './template/NotFound.tsx';
-
-const router = createBrowserRouter([
-  {path: "/", element: <App />},
-  {path: "/register", element: <Register />},
-  {path: "/login", element: <Login />},
-  {path: "*", element: <NotFound />}
-])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
