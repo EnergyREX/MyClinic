fa0425999082b2c89e8c73b61e00b4e5a8e16b13
EnergyREX <energy.rex.2003@gmail.com>
2025-05-31 08:46:14 +0200

    feat: internationalization

--- frontend/src/Components/organisms/Sidenav.tsx
+++ frontend/src/Components/organisms/Sidenav.tsx
@@ -5,70 +5,76 @@ import SidenavFooter from '../molecules/SidenavFooter'
 import Typography from '../atoms/Typography'
 import { Link } from 'react-router-dom';
 import { Book, Bot, Building, Container, CreditCard, NotepadText, Pill, TestTubes, TicketCheck, Users, Warehouse } from 'lucide-react'
+import Divider from '../atoms/Divider'
+import { useTranslation } from 'react-i18next'
 
 const Sidenav = () => {
+
+  const { t } = useTranslation('navigation');
+
   return (
-    <nav>
+    <nav className='h-dvh flex flex-col bg-neutral-900 px-2'>
       <SidenavHeader>
         <Link to={"/"}>
         <Typography variant='h1'>MyClinic</Typography>
+        <Divider />
         </Link>
       </SidenavHeader>
 
       <SidenavBody>
         <Link to={"/appointments"} className='flex gap-1 align-middle'>
           <Users />
-          <Typography variant='h3'>Appointments</Typography>
+          <Typography variant='h3'>{t('navigation.appointments')}</Typography>
         </Link>
 
         <Link to={"/departments"} className='flex gap-1 align-middle'>
-          <Building />
-          <Typography variant='h3'>Departments</Typography>
+          <Building className=''/>
+          <Typography variant='h3'>{t('navigation.departments')}</Typography>
         </Link>
 
         <Link to={"/doctors"} className='flex gap-1 align-middle'>
           <Pill />
-          <Typography variant='h3'>Doctors</Typography>
+          <Typography variant='h3'>{t('navigation.doctors')}</Typography>
         </Link>
 
         <Link to={"/inventories"} className='flex gap-1 align-middle'>
           <Warehouse />
-          <Typography variant='h3'>Inventories</Typography>
+          <Typography variant='h3'>{t('navigation.inventories')}</Typography>
         </Link>
 
         <Link to={"/invoices"} className='flex gap-1 align-middle'>
           <CreditCard />
-          <Typography variant='h3'>Invoices</Typography>
+          <Typography variant='h3'>{t('navigation.invoices')}</Typography>
         </Link>
 
         <Link to={"/access-tokens"} className='flex gap-1 align-middle'>
           <TicketCheck />
-          <Typography variant='h3'>Access Tokens</Typography>
+          <Typography variant='h3'>{t('navigation.access-tokens')}</Typography>
         </Link>
 
         <Link to={"/medical-records"} className='flex gap-1 align-middle'>
           <NotepadText />
-          <Typography variant='h3'>Medical Records</Typography>
+          <Typography variant='h3'>{t('navigation.medical-records')}</Typography>
         </Link>
 
         <Link to={"/roles"} className='flex gap-1 align-middle'>
           <Book />
-          <Typography variant='h3'>Roles</Typography>
+          <Typography variant='h3'>{t('navigation.roles')}</Typography>
         </Link>
 
         <Link to={"/suppliers"} className='flex gap-1 align-middle'>
           <Container />
-          <Typography variant='h3'>Suppliers</Typography>
+          <Typography variant='h3'>{t('navigation.suppliers')}</Typography>
         </Link>
 
         <Link to={"/treatments"} className='flex gap-1 align-middle'>
           <TestTubes />
-          <Typography variant='h3'>Treatments</Typography>
+          <Typography variant='h3'>{t('navigation.treatments')}</Typography>
         </Link>
 
         <Link to={"/treatments"} className='flex gap-1 align-middle'>
           <Bot />
-          <Typography variant='h3'>Auto-diagnosis</Typography>
+          <Typography variant='h3'>{t('navigation.auto-diagnosis')}</Typography>
         </Link>
       </SidenavBody>
 
