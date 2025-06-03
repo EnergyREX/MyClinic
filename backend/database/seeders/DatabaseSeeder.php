84605a6ce3bca58a7d1b1454d19b442bd1fe7880
EnergyREX <energy.rex.2003@gmail.com>
2025-06-03 09:50:04 +0200

    refactor: added more all the permissions in api route

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -27,15 +27,62 @@ class DatabaseSeeder extends Seeder
         }
 
         $permissions = [
-            'view_appointments',
-            'view_roles',
-            'view_inventories',
-            'view_suppliers',
+            'view_departments',
+            'view_single_department',
+            'create_departments',
+            'update_departments',
+            'delete_departments',
+
             'view_doctors',
-            'view_statuses',
-            'view_medicalrecords',
-            'view_medicalrecords-Own',
-            'view_treatments'
+            'view_single_doctor',
+            'create_doctors',
+            'update_doctors',
+            'delete_doctors',
+
+            'view_appointments',
+            'view_single_appointment',
+            'create_appointment',
+            'update_appointment',
+            'delete_appointment',
+
+            'get_tokens',
+            'delete_token',
+
+            'view_roles',
+            'view_single_role',
+            'create_role',
+            'update_role',
+            'delete_role',
+
+            'view_inventories',
+            'view_single_item',
+            'create_inventory',
+            'update_inventory',
+            'delete_inventory',
+
+            'view_suppliers',
+            'view_single_suppliers',
+            'create_suppliers',
+            'update_suppliers',
+            'delete_suppliers',
+
+            'view_invoices',
+            'view_single_invoices',
+            'create_invoices',
+            'update_invoices',
+            'delete_invoices',
+
+            'view_treatments',
+            'view_single_treatments',
+            'create_treatments',
+            'update_treatments',
+            'delete_treatments',
+
+            'view_records',
+            'view_single_records',
+            'create_records',
+            'update_records',
+            'delete_records',
         ];
 
         foreach ($permissions as $perm) {
@@ -47,16 +94,19 @@ class DatabaseSeeder extends Seeder
         $allRoles['user']->permission()->sync([
             $allPermissions['view_appointments']->id,
             $allPermissions['view_inventories']->id,
-            $allPermissions['view_medicalrecords']->id,
+            $allPermissions['view_records']->id,
             $allPermissions['view_roles']->id,
             $allPermissions['view_treatments']->id,
         ]);
 
-        // User::factory(10)->create();
+        User::factory()->create([
+            'email' => 'myclinic@myclinic.com',
+            'password' => 'myclinic'
+        ]);
 
         User::factory()->create([
-            'email' => 'test@example.com',
-            'password' => 'password'
+            'email' => 'admin@myclinic.com',
+            'password' => 'admin'
         ]);
     }
 }
