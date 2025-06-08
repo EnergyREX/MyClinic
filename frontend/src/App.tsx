c3e1c56b1ff309e2dd50ae84d6e73ed0efd309a4
EnergyREX <energy.rex.2003@gmail.com>
2025-06-08 23:59:28 +0200

    feat: add Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,21 @@ import Home from './template/Home.tsx';
 import Register from './template/Register';
 import Login from './template/Login';
 import NotFound from './template/NotFound.tsx';
-import ProtectedRoute from './Components/atoms/ProtectedRoute.tsx';
-import ProtectedRoutes from './Components/organisms/ProtectedRoutes.tsx';
-import { CrudPage } from './template/CrudPage.tsx';
+import ProtectedRoute from './Components/auth/ProtectedRoute.tsx';
+import ProtectedRoutes from './Components/auth/ProtectedRoutes.tsx';
+import CrudPage from './template/CrudPage.tsx';
 import CrudCard from './template/CrudCard.tsx';
 import useUserData from './hooks/useUserData.tsx';
-import Departments from './pages/Departments.tsx';
+import Departments from './pages/Departments/Departments.tsx';
+import Appointments from './pages/Appointments/Appointments.tsx';
+import AccessTokens from './pages/AccessTokens/AccessTokens.tsx';
+import Doctors from './pages/Doctors/Doctors.tsx';
+import Inventories from './pages/Inventories/Inventories.tsx';
+import Invoices from './pages/Invoices/Invoices.tsx';
+import MedicalRecords from './pages/MedicalRecords/MedicalRecords.tsx';
+import Roles from './pages/Roles/Roles.tsx';
+import Suppliers from './pages/Suppliers/Suppliers.tsx';
+import Treatments from './pages/Treatments/Treatments.tsx';
 
 const App = () => {
   const { permissions, recoverPermissions, clearPermissions } = useUserData()
@@ -34,14 +43,16 @@ const App = () => {
         <Route path='*' element={<NotFound />} />
 
         <Route element={<ProtectedRoutes permission='view_roles'/>}>
+          <Route path='/access-tokens' element={<AccessTokens />}/>
+          <Route path='/appointments' element={<Appointments />}/>
           <Route path='/departments' element={<Departments />}/>
-          <Route path='/appointments' element={<CrudPage />}/>
-          <Route path='/doctors' element={<CrudPage />}/>
-          <Route path='/inventories' element={<CrudPage />}/>
-          <Route path='/invoices' element={<CrudPage />}/>
-          <Route path='/jwt-tokens' element={<CrudPage />}/>
-          <Route path='/medical-records' element={<CrudPage />}/>
-          <Route path='/crud-card' element={<ProtectedRoute permission='view_roles' element={<CrudCard />}/>} />
+          <Route path='/doctors' element={<Doctors />}/>
+          <Route path='/inventories' element={<Inventories />}/>
+          <Route path='/invoices' element={<Invoices />}/>
+          <Route path='/medical-records' element={<MedicalRecords />}/>
+          <Route path='/suppliers' element={<Suppliers />}/>
+          <Route path='/treatments' element={<Treatments />}/>
+          <Route path='/roles' element={<Roles />} />
           
         </Route>
       </Routes>
