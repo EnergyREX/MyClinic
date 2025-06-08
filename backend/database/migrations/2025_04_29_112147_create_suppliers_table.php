c24cab7b64571801dad4569bbbf7a6788d0d81a5
EnergyREX <energy.rex.2003@gmail.com>
2025-06-08 23:57:06 +0200

    refactor: fixed a typo

--- backend/database/migrations/2025_04_29_112147_create_suppliers_table.php
+++ backend/database/migrations/2025_04_29_112147_create_suppliers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name', 255);
             $table->text('description')->nullable();
-            $table->string('direction', 255);
+            $table->string('address', 255);
             $table->timestamps();
         });
     }
