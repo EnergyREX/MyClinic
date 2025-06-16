10888719843148a52859e3fbf42722688dc038d7
EnergyREX <energy.rex.2003@gmail.com>
2025-06-16 22:54:07 +0200

    docs: comments in controller

--- backend/app/Http/Controllers/JwtTokenController.php
+++ backend/app/Http/Controllers/JwtTokenController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JwtToken;
 use Illuminate\Http\Request;
 
 class JwtTokenController extends Controller
 {
-    //
+    // Show all tokens
+    function index() {
+    $status = JwtToken::all();
+        return response()->json([
+            'success' => true,
+            'data' => $status
+        ]);
+    }
+
+    // Delete a token by ID
+    function destroy($id) {
+        $status = JwtToken::findOrFail($id);
+        $status->delete();
+    
+        return response()->json([
+            'success' => true,
+            'message' => "Status eliminated"
+        ]);
+    }
+
+    // TODO: Show own user tokens + permission
 }
