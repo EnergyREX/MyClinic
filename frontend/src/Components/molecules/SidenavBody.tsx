539f3388b80efb79ee7dbe6155371ff44ef0c21c
EnergyREX <energy.rex.2003@gmail.com>
2025-06-16 23:19:07 +0200

    feat: render depending of permissions

--- frontend/src/Components/molecules/SidenavBody.tsx
+++ frontend/src/Components/molecules/SidenavBody.tsx
@@ -1,46 +1,47 @@
-import React, { ReactNode } from 'react'
+import React from 'react';
 import Link from './Link';
 import { Book, Bot, Building, Container, CreditCard, NotepadText, Pill, TestTubes, TicketCheck, Users, Warehouse } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
+import Permission from '../auth/Permission';
 
 const SidenavBody = () => {
-    const { t } = useTranslation('navigation');
+  const { t } = useTranslation('navigation');
 
   return (
     <div className='flex-grow'>
-        <Link to="/appointments" label={t('navigation.appointments')}>
-          <Users />
-        </Link>
+      <Permission requiredPermission="view_appointments">
+        <Link to="/appointments" label={t('navigation.appointments')}><Users /></Link>
+      </Permission>
 
-        <Link to="/departments" label={t('navigation.departments')}>
-          <Building />
-        </Link>
+      <Permission requiredPermission="view_departments">
+        <Link to="/departments" label={t('navigation.departments')}><Building /></Link>
+      </Permission>
 
-        <Link to="/doctors" label={t('navigation.doctors')}>
-          <Pill />
-        </Link>
+      <Permission requiredPermission="view_doctors">
+        <Link to="/doctors" label={t('navigation.doctors')}><Pill /></Link>
+      </Permission>
 
-        <Link to="/inventories" label={t('navigation.inventories')}>
-          <Warehouse />
-        </Link>
+      <Permission requiredPermission="view_inventories">
+        <Link to="/inventories" label={t('navigation.inventories')}><Warehouse /></Link>
+      </Permission>
 
-        <Link to="/jwt-tokens" label={t('navigation.access-tokens')}>
-          <TicketCheck />
-        </Link>
+      <Permission requiredPermission="view_jwttokens">
+        <Link to="/jwt-tokens" label={t('navigation.access-tokens')}><TicketCheck /></Link>
+      </Permission>
 
-        <Link to="/medical-records" label={t('navigation.medical-records')}>
-          <NotepadText />
-        </Link>
+      <Permission requiredPermission="view_medical_records">
+        <Link to="/medical-records" label={t('navigation.medical-records')}><NotepadText /></Link>
+      </Permission>
 
-        <Link to="/suppliers" label={t('navigation.suppliers')}>
-          <Container />
-        </Link>
+      <Permission requiredPermission="view_suppliers">
+        <Link to="/suppliers" label={t('navigation.suppliers')}><Container /></Link>
+      </Permission>
 
-        <Link to="/treatments" label={t('navigation.treatments')}>
-          <TestTubes />
-        </Link>
+      <Permission requiredPermission="view_treatments">
+        <Link to="/treatments" label={t('navigation.treatments')}><TestTubes /></Link>
+      </Permission>
     </div>
-  )
-}
+  );
+};
 
-export default SidenavBody
+export default SidenavBody;
