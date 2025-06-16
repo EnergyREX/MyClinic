af9d04cdafe0b195a56a9373cf1c7007c85bff52
EnergyREX <energy.rex.2003@gmail.com>
2025-06-16 12:44:32 +0200

    changes in routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,14 +10,12 @@ import ProtectedRoutes from './Components/auth/ProtectedRoutes.tsx';
 import useUserData from './hooks/useUserData.tsx';
 import Departments from './pages/Departments/Departments.tsx';
 import Appointments from './pages/Appointments/Appointments.tsx';
-import AccessTokens from './pages/AccessTokens/AccessTokens.tsx';
 import Doctors from './pages/Doctors/Doctors.tsx';
 import Inventories from './pages/Inventories/Inventories.tsx';
-import Invoices from './pages/Invoices/Invoices.tsx';
 import MedicalRecords from './pages/MedicalRecords/MedicalRecords.tsx';
-import Roles from './pages/Roles/Roles.tsx';
 import Suppliers from './pages/Suppliers/Suppliers.tsx';
 import Treatments from './pages/Treatments/Treatments.tsx';
+import JWTTokens from './pages/JWTTokens/JWTTokens.tsx';
 
 const App = () => {
   const { permissions, recoverPermissions, clearPermissions } = useUserData()
@@ -41,16 +39,14 @@ const App = () => {
         <Route path='*' element={<NotFound />} />
 
         <Route element={<ProtectedRoutes permission='view_roles'/>}>
-          <Route path='/access-tokens' element={<AccessTokens />}/>
+          <Route path='/jwt-tokens' element={<JWTTokens />}/>
           <Route path='/appointments' element={<Appointments />}/>
           <Route path='/departments' element={<Departments />}/>
           <Route path='/doctors' element={<Doctors />}/>
           <Route path='/inventories' element={<Inventories />}/>
-          <Route path='/invoices' element={<Invoices />}/>
           <Route path='/medical-records' element={<MedicalRecords />}/>
           <Route path='/suppliers' element={<Suppliers />}/>
           <Route path='/treatments' element={<Treatments />}/>
-          <Route path='/roles' element={<Roles />} />
           
         </Route>
       </Routes>
