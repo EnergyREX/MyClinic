25897d8115c64ebd56f3c95228b5aabd8c163bb1
EnergyREX <energy.rex.2003@gmail.com>
2025-06-17 02:41:25 +0200

    feat: specific permission

--- frontend/src/pages/Appointments/Appointments.tsx
+++ frontend/src/pages/Appointments/Appointments.tsx
@@ -2,47 +2,54 @@ import React, { useMemo } from 'react'
 import CrudPage from '../../Components/template/CrudPage'
 import { useTranslation } from 'react-i18next';
 import { fields } from './formFields';
+import { Permissions } from '../../types/permissions';
 
 
 const Appointments = () => {
 
   const { t } = useTranslation('columndefs')
 
-  console.log(t(''))
+  const permissions: Permissions = {
+    create: 'create_appointments',
+    read: 'view_appointments',
+    update: 'update_appointments',
+    delete: 'delete_appointments',
+  }
 
-const columns = useMemo(() => [
-  {
-  id: 'select',
-  header: ({ table }) => (
-    <input
-      type="checkbox"
-      checked={table.getIsAllPageRowsSelected()}
-      onChange={table.getToggleAllPageRowsSelectedHandler()}
-    />
-  ),
-  cell: ({ row }) => (
-    <input
-      type="checkbox"
-      checked={row.getIsSelected()}
-      disabled={!row.getCanSelect()}
-      onChange={row.getToggleSelectedHandler()}
-    />
-  ),
-  enableSorting: false,
-  enableColumnFilter: false,
-  },
-  { id: 'id', header: t('appointments.id'), accessorKey: 'id' },
-  { id: 'patient_dni', header: t('appointments.patient_dni'), accessorKey: 'patient_dni' },
-  { id: 'doctor_dni', header: t('appointments.doctor_dni'), accessorKey: 'doctor_dni' },
-  { id: 'status_id', header: t('appointments.status_id'), accessorKey: 'status' },
-  { id: 'hour', header: t('appointments.hour'), accessorKey: 'hour' },
-  { id: 'date', header: t('appointments.date'), accessorKey: 'date' },
-], [t])
+  const columns = useMemo(() => [
+    {
+    id: 'select',
+    header: ({ table }) => (
+      <input
+        type="checkbox"
+        checked={table.getIsAllPageRowsSelected()}
+        onChange={table.getToggleAllPageRowsSelectedHandler()}
+      />
+    ),
+    cell: ({ row }) => (
+      <input
+        type="checkbox"
+        checked={row.getIsSelected()}
+        disabled={!row.getCanSelect()}
+        onChange={row.getToggleSelectedHandler()}
+      />
+    ),
+    enableSorting: false,
+    enableColumnFilter: false,
+    },
+    { id: 'id', header: t('appointments.id'), accessorKey: 'id' },
+    { id: 'patient_dni', header: t('appointments.patient_dni'), accessorKey: 'patient_dni' },
+    { id: 'doctor_dni', header: t('appointments.doctor_dni'), accessorKey: 'doctor_dni' },
+    { id: 'status_id', header: t('appointments.status_id'), accessorKey: 'status' },
+    { id: 'hour', header: t('appointments.hour'), accessorKey: 'hour' },
+    { id: 'date', header: t('appointments.date'), accessorKey: 'date' },
+  ], [t])
 
 
 
   return (
     <CrudPage 
+    permissions={permissions}
     columns={columns}
     formFields={fields}
     />
