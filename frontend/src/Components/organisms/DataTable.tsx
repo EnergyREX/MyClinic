c81129e3c4d75db5f2b2ad70994058f1927656be
EnergyREX <energy.rex.2003@gmail.com>
2025-06-17 02:40:31 +0200

    feat: render depending on permissions

--- frontend/src/Components/organisms/DataTable.tsx
+++ frontend/src/Components/organisms/DataTable.tsx
@@ -6,12 +6,26 @@ import DataTableBody from '../molecules/DataTableBody'
 import Pagination from '../molecules/Pagination'
 import DataTableToolbar from '../molecules/DataTableToolbar'
 
-const DataTable = ({ table, openCreate, openDelete, reload }) => {
+interface props {
+  table: Table<any>
+  openCreate: () => void
+  openDelete: () => void
+  reload: () => void
+  permissions?: {
+    create?: string
+    read?: string
+    update?: string
+    delete?: string
+  }
+}
+
+const DataTable = ({ table, openCreate, openDelete, reload, permissions }: props) => {
   
   
   return (
     <>
       <DataTableToolbar 
+      permissions={permissions}
       openCreate={openCreate}
       openDelete={openDelete}
       reload={reload}
