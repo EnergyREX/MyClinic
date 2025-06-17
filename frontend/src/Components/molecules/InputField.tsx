6ad46f42b257239f4783bb3196448fb70049efa0
EnergyREX <energy.rex.2003@gmail.com>
2025-06-17 22:27:46 +0200

    style: deleted margin

--- frontend/src/Components/molecules/InputField.tsx
+++ frontend/src/Components/molecules/InputField.tsx
@@ -4,7 +4,7 @@ import Input from '../atoms/Input'
 
 const InputField = ({ label, name, ...props }) => {
   return (
-    <div className='flex flex-col gap-1 mb-4'>
+    <div className='flex flex-col gap-1'>
       <Label>{label}</Label>
       <Input name={name} {...props} />
     </div>
