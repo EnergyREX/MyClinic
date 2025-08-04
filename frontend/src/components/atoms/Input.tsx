5c46aa35e74761c898dff4be1dee24fa399f0748
Francisco Javier Martín Fernández <energy.rex.2003@gmail.com>
2025-08-04 19:51:14 +0200

    style: add animations to input component

--- frontend/src/components/atoms/Input.tsx
+++ frontend/src/components/atoms/Input.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from "react";
 
 interface props extends React.InputHTMLAttributes<HTMLInputElement> {
-  className?: string,
+  className?: string;
 }
 
 const Input = ({ className, ...props }: props) => {
+  const styles: string = "border-1 border-neutral-400 p-1 rounded-sm";
 
-  const styles: string = "border-1 p-1 rounded-md"
+  const animations: string =
+    "duration-200 focus:border-neutral-300 focus:shadow-md shadow-neutral-700 dark:shadow-neutral-200/20 hover:shadow-md";
 
   return (
-    <input className={`${styles} ${className}`} {...props} />
-  )
-}
+    <input className={`${styles} ${animations} ${className}`} {...props} />
+  );
+};
 
-export default Input
+export default Input;
