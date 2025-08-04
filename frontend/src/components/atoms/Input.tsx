abebb9721b5751b778f0e5bc06e6e3e6d67703fc
Francisco Javier Martín Fernández <energy.rex.2003@gmail.com>
2025-08-04 20:16:39 +0200

    style: added some padding

--- frontend/src/components/atoms/Input.tsx
+++ frontend/src/components/atoms/Input.tsx
@@ -5,7 +5,7 @@ interface props extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input = ({ className, ...props }: props) => {
-  const styles: string = "border-1 border-neutral-400 p-1 rounded-sm";
+  const styles: string = "border-1 border-neutral-400 p-1.5 px-3 rounded-sm";
 
   const animations: string =
     "duration-200 focus:border-neutral-300 focus:shadow-md shadow-neutral-700 dark:shadow-neutral-200/20 hover:shadow-md";
