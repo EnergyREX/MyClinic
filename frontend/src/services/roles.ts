6160e6e010c71bcbe102f7c27ecaa67f33c9869f
Francisco Javier Martín Fernández <energy.rex.2003@gmail.com>
2025-08-08 15:40:08 +0200

    update: services

---
+++ frontend/src/services/roles.ts
@@ -0,0 +1,6 @@
+// This service aims to request to the server all roles to after, render it.
+
+function getRoles() {}
+
+// This function aims to get the permissions with their data
+function getPermissions() {}
